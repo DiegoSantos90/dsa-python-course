5e37bb5b8502a997de67eaecadb11c90c71ee3b2
Diego Rodolfo dos Santos <dr.santos1990@gmail.com>
2025-06-17 16:36:58 -0300

    Prep Uber

---
+++ prep-uber/LeetCode-Uber/BFS/bus-routes.py
@@ -0,0 +1,74 @@
+'''
+815. Bus Routes
+Solved
+Hard
+Topics
+conpanies icon
+Companies
+You are given an array routes representing bus routes where routes[i] is a bus route that the ith bus repeats forever.
+For example, if routes[0] = [1, 5, 7],
+this means that the 0th bus travels in the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
+You will start at the bus stop source (You are not on any bus initially),
+and you want to go to the bus stop target. You can travel between bus stops by buses only.
+Return the least number of buses you must take to travel from source to target.
+Return -1 if it is not possible.
+
+Example 1:
+Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+Output: 2
+Explanation: The best strategy is take the first bus to the bus stop 7,
+then take the second bus to the bus stop 6.
+
+Example 2:
+Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+Output: -1
+
+Constraints:
+1 <= routes.length <= 500.
+1 <= routes[i].length <= 105
+All the values of routes[i] are unique.
+sum(routes[i].length) <= 105
+0 <= routes[i][j] < 106
+0 <= source, target < 106
+'''
+from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+
+        if source == target:
+            return 0
+
+        graph = defaultdict(set)
+        print(graph)
+        queue = deque([(source, 0)])
+
+        for bus, route in enumerate(routes):
+            for stop in route:
+                graph[stop].add(bus)  # map stop to bus index
+
+        visited_stops = set()
+        visited_buses = set()
+
+        while queue:
+            stop, route_len = queue.popleft()
+
+            if stop == target:
+                return route_len
+
+            for bus in graph[stop]:
+                if bus not in visited_buses:
+                    visited_buses.add(bus)
+                    for stop in routes[bus]:
+                        if stop not in visited_stops:
+                            visited_stops.add(stop)
+                            queue.append((stop, route_len + 1))
+
+        return -1  # If we exhaust the queue without finding the target
+
+if __name__ == "__main__":
+    # Example usage
+    solution = Solution()
+    print(f"expected: 2 actual: {solution.numBusesToDestination([[1, 2, 7], [3, 6, 7]], 1, 6)} \n")  # Output: 2
+    #print(f"expected: -1 actual: {solution.numBusesToDestination([[7, 12], [4, 5, 15], [6], [15, 19], [9, 12, 13]], 15, 12)} \n")  # Output: -1
---
+++ prep-uber/LeetCode-Uber/Binary-Search/random-pick-with-weight.py
@@ -0,0 +1,82 @@
+'''
+528. Random Pick with Weight
+You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+You need to implement the function pickIndex(), which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. The probability of picking an index i is w[i] / sum(w).
+For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%),
+and the probability of picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+
+Example 1:
+Input
+["Solution","pickIndex"]
+[[[1]],[]]
+Output
+[null,0]
+Explanation
+Solution solution = new Solution([1]);
+solution.pickIndex(); // return 0. The only option is to return 0 since there is only one element in w.
+
+Example 2:
+Input
+["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+[[[1,3]],[],[],[],[],[]]
+Output
+[null,1,1,1,1,0]
+
+Explanation
+Solution solution = new Solution([1, 3]);
+solution.pickIndex(); // return 1. It is returning the second element (index = 1) that has a probability of 3/4.
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 0. It is returning the first element (index = 0) that has a probability of 1/4.
+
+Since this is a randomization problem, multiple answers are allowed.
+All of the following outputs can be considered correct:
+[null,1,1,1,1,0]
+[null,1,1,1,1,1]
+[null,1,1,1,0,0]
+[null,1,1,1,0,1]
+[null,1,0,1,0,0]
+......
+and so on.
+
+Constraints:
+1 <= w.length <= 104
+1 <= w[i] <= 105
+pickIndex will be called at most 104 times.
+'''
+import random
+from typing import List
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix_sums = []
+        total = 0
+
+        for weight in w:
+            total += weight
+            self.prefix_sums.append(total)
+
+        self.total = total
+
+    def pickIndex(self) -> int:
+        # [1, 2, 4] = [1, 3, 7] --> 4
+        target = random.uniform(0, self.total)
+
+        l = 0
+        r = len(self.prefix_sums)
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if self.prefix_sums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+if __name__ == "__main__":
+    #Your Solution object will be instantiated and called as such:
+    obj = Solution([1, 3])
+    print(obj.pickIndex())
